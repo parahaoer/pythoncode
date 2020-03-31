2360150a1253a682d81f8cf5e07aef34acf6df42
parahaoer <884101054@qq.com>
2020-03-31 16:41:16 +0800

    在字符串前面加上r, 使字符串内容不被转义

---
+++ UploadDataToES/test/testHoldEscape.py
@@ -0,0 +1,6 @@
+rule_1_list = [r'name=\"Command\"; value=\"route print\"', r'name=\"Command\"; value=\"ipconfig /all\"']
+
+for rule in rule_1_list:
+    print(rule)
+
+rule_1_list = [r"name=\"Command\"; value=\"route print\"", r"name=\"Command\"; value=\"ipconfig /all\""]
